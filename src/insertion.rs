1d705a6f6e97b18870c36f029cdfa5ff4ec0eede
Maciej Szybiak <maciek.szybiak@gmail.com>
2020-10-04 22:50:20 +0200

    Added insertion sort and fixed benchmark function.

---
+++ src/insertion.rs
@@ -0,0 +1,24 @@
+pub fn sort(array: &mut [i32]) {
+    let length = array.len();
+    let mut j: usize;
+    
+    for i in 1..length {
+        j = i;
+        while j > 0 && array[j] < array[j - 1] {
+            array.swap(j, j - 1);
+            j -= 1;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use sorting_algorithms::{UNSORTED_ARRAY, SORTED_ARRAY};
+    #[test]
+    fn test_insertion_sort() {
+        let mut arr = UNSORTED_ARRAY;
+        sort(&mut arr);
+        assert_eq!(arr, SORTED_ARRAY);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 use std::time::Instant;
+use rand::Rng;
 
 mod bubble;
+mod insertion;
 
-const ARR_SIZE: usize = 100000;
+const ARR_SIZE: usize = 1000;
 
 fn benchmark(f: &dyn Fn(&mut [i32]), name: &str) {
+    let mut rng = rand::thread_rng();
     let mut arr: [i32; ARR_SIZE] = [0; ARR_SIZE];
     for i in 0..ARR_SIZE {
-        arr[i] = i as i32 + 1;
+        arr[i] = rng.gen::<i32>();
     }
     print!("Benchmarking {}...", name);
 
@@ -20,6 +23,7 @@ fn run_benchmarks() {
     println!("Running benchmarks for {} array elements...", ARR_SIZE);
 
     benchmark(&bubble::sort, "bubble sort");
+    benchmark(&insertion::sort, "insertion sort");
 }
 
 fn main() {
